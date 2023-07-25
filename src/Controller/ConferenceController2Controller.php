92fec63e63d48e2531f9af46979c501ab5479236
Harlojuice <aguall60@gmail.com>
2023-07-25 05:00:41 +0300

    hello

--- src/Controller/ConferenceController2Controller.php
+++ src/Controller/ConferenceController2Controller.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-//hello
+
 class ConferenceController2Controller extends AbstractController
 {
     #[Route('/', name: 'homepage')]
