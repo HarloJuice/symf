c75c4f2513eb9cb1992bb0ca211ba7170320c25f
unknown <aguall60@gmail.com>
2023-07-28 03:13:36 +0300

    fix problems with templates

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,17 +3,27 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/conference1', name: 'app_conference')]
-    public function index(): JsonResponse
+    #[Route('/', name: 'homepage')]
+    public function index(Request $request): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ConferenceController.php',
-        ]);
+        $greet = '';
+        if($name = $request->query->get('hello')){
+            $greet = $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
+            return new Response(<<<EOF
+            <html>
+                <body>
+                $greet
+                    <img src="/images/image001.gif" />
+                </body>
+            </html>
+            EOF
+        );
     }
 }
